28e60d48d0323c82fa3e6f3fc55479f45d33a5be
Jawaun <jawaun.brown95@gmail.com>
2018-10-08 16:01:43 -0400

    enabled jpa auditing

--- ez-note/src/main/java/com/example/eznote/EzNoteApplication.java
+++ ez-note/src/main/java/com/example/eznote/EzNoteApplication.java
@@ -2,8 +2,11 @@ package com.example.eznote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class EzNoteApplication {
 
 	public static void main(String[] args) {
