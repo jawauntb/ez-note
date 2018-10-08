b0a48bc20983f6049443703aaa16d60cc775b85d
Jawaun <jawaun.brown95@gmail.com>
2018-10-08 14:27:32 -0400

    This is a maven wrapper for the spring application. It will load all the dependecies and allow for building, formatting etc of the application.

---
+++ ez-note/src/main/java/com/example/eznote/EzNoteApplication.java
@@ -0,0 +1,12 @@
+package com.example.eznote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzNoteApplication.class, args);
+	}
+}
