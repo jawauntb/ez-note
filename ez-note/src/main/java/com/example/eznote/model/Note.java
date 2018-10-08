69266579e6237b1738703cfae603283bfc0cf8fb
Jawaun <jawaun.brown95@gmail.com>
2018-10-08 15:59:21 -0400

    getters and setters for note app

--- ez-note/src/main/java/com/example/eznote/model/Note.java
+++ ez-note/src/main/java/com/example/eznote/model/Note.java
@@ -34,4 +34,44 @@ public class Note implements Serializable {
     @LastModifiedDate
     private Date updatedAt;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
